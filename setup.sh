1c5595da6b5abdaf94c6b9798b948de141ded4cc
Amrish Singh <beforebeta@gmail.com>
2013-09-14 09:26:01 -0400

    setup script

---
+++ setup.sh
@@ -0,0 +1,13 @@
+sudo apt-get update
+sudo apt-get install apache2
+sudo apt-get install mysql-server libapache2-mod-auth-mysql php5-mysql
+sudo mysql_install_db
+sudo /usr/bin/mysql_secure_installation
+sudo apt-get install apache2.2-common apache2-mpm-prefork apache2-utils libexpat1 ssl-cert
+sudo apt-get install libapache2-mod-wsgi
+sudo apt-get install python-pip
+pip install Django
+sudo apt-get install python-dev
+sudo apt-get install libmysqlclient-dev
+pip install mysql-python
+pip install requests
