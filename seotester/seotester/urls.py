c6a01d416f4276b3a34ff0769b4ee217b011c0fe
Amrish Singh <beforebeta@gmail.com>
2014-03-14 00:34:25 -0400

    must be admin user check for seotester

--- seotester/seotester/urls.py
+++ seotester/seotester/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 #Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -16,3 +17,10 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('seotester.web.urls')),
 )
+
+
+if not settings.DEBUG:
+    urlpatterns += patterns('',
+            url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
+
