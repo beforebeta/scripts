548a3f00043438411ada8b389c4a02f7f3818b0a
Amrish Singh <beforebeta@gmail.com>
2014-03-27 14:27:29 -0400

    testing framework

--- tester/tester/main/migrations/0001_initial.py
+++ tester/tester/main/migrations/0001_initial.py
@@ -8,15 +8,122 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        # Adding field 'TestGroupingRun.user'
-        db.add_column(u'main_testgroupingrun', 'user',
-                      self.gf('django.db.models.fields.related.ForeignKey')(default=None, to=orm['auth.User']),
-                      keep_default=False)
+        # Adding model 'Application'
+        db.create_table(u'main_application', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=255)),
+        ))
+        db.send_create_signal(u'main', ['Application'])
+
+        # Adding model 'TestStep'
+        db.create_table(u'main_teststep', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('testcase', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['main.TestCase'])),
+            ('sequence', self.gf('django.db.models.fields.IntegerField')(default=1)),
+            ('description', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
+            ('expected_result', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
+            ('expected_result_screenshot', self.gf('django.db.models.fields.URLField')(max_length=255, null=True, blank=True)),
+            ('date_added', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime(2014, 3, 27, 0, 0), auto_now_add=True, blank=True)),
+            ('last_modified', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime(2014, 3, 27, 0, 0), auto_now=True, auto_now_add=True, blank=True)),
+        ))
+        db.send_create_signal(u'main', ['TestStep'])
+
+        # Adding model 'TestCase'
+        db.create_table(u'main_testcase', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('application', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['main.Application'])),
+            ('grouping', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['main.TestGrouping'])),
+            ('sequence', self.gf('django.db.models.fields.IntegerField')(default=1)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('description', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
+            ('preconditions', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
+            ('comments', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
+            ('date_added', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime(2014, 3, 27, 0, 0), auto_now_add=True, blank=True)),
+            ('last_modified', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime(2014, 3, 27, 0, 0), auto_now=True, auto_now_add=True, blank=True)),
+        ))
+        db.send_create_signal(u'main', ['TestCase'])
+
+        # Adding model 'TestGrouping'
+        db.create_table(u'main_testgrouping', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('application', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['main.Application'])),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('date_added', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime(2014, 3, 27, 0, 0), auto_now_add=True, blank=True)),
+            ('last_modified', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime(2014, 3, 27, 0, 0), auto_now=True, auto_now_add=True, blank=True)),
+        ))
+        db.send_create_signal(u'main', ['TestGrouping'])
+
+        # Adding model 'TestStepRun'
+        db.create_table(u'main_teststeprun', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('teststep', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['main.TestStep'])),
+            ('testcase_run', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['main.TestCaseRun'])),
+            ('sequence', self.gf('django.db.models.fields.IntegerField')(default=1)),
+            ('description', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
+            ('expected_result', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
+            ('expected_result_screenshot', self.gf('django.db.models.fields.URLField')(max_length=255, null=True, blank=True)),
+            ('status', self.gf('django.db.models.fields.CharField')(default='NT', max_length=2)),
+            ('actual_result', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
+            ('actual_result_screenshot', self.gf('django.db.models.fields.URLField')(max_length=255, null=True, blank=True)),
+            ('date_added', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime(2014, 3, 27, 0, 0), auto_now_add=True, blank=True)),
+            ('last_modified', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime(2014, 3, 27, 0, 0), auto_now=True, auto_now_add=True, blank=True)),
+        ))
+        db.send_create_signal(u'main', ['TestStepRun'])
+
+        # Adding model 'TestCaseRun'
+        db.create_table(u'main_testcaserun', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('testcase', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['main.TestCase'])),
+            ('testgrouping_run', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['main.TestGroupingRun'])),
+            ('sequence', self.gf('django.db.models.fields.IntegerField')(default=1)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('description', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
+            ('preconditions', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
+            ('comments', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
+            ('tester_comments', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
+            ('overall_status', self.gf('django.db.models.fields.CharField')(default='Incomplete', max_length=50, null=True, blank=True)),
+            ('date_added', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime(2014, 3, 27, 0, 0), auto_now_add=True, blank=True)),
+            ('last_modified', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime(2014, 3, 27, 0, 0), auto_now=True, auto_now_add=True, blank=True)),
+        ))
+        db.send_create_signal(u'main', ['TestCaseRun'])
+
+        # Adding model 'TestGroupingRun'
+        db.create_table(u'main_testgroupingrun', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'])),
+            ('testgrouping', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['main.TestGrouping'])),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('browser', self.gf('django.db.models.fields.CharField')(max_length=10)),
+            ('user_agent', self.gf('django.db.models.fields.CharField')(max_length=255, null=True, blank=True)),
+            ('ip_address', self.gf('django.db.models.fields.CharField')(max_length=255, null=True, blank=True)),
+            ('overall_status', self.gf('django.db.models.fields.CharField')(default='Incomplete', max_length=50, null=True, blank=True)),
+            ('date_added', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime(2014, 3, 27, 0, 0), auto_now_add=True, blank=True)),
+            ('last_modified', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime(2014, 3, 27, 0, 0), auto_now=True, auto_now_add=True, blank=True)),
+        ))
+        db.send_create_signal(u'main', ['TestGroupingRun'])
 
 
     def backwards(self, orm):
-        # Deleting field 'TestGroupingRun.user'
-        db.delete_column(u'main_testgroupingrun', 'user_id')
+        # Deleting model 'Application'
+        db.delete_table(u'main_application')
+
+        # Deleting model 'TestStep'
+        db.delete_table(u'main_teststep')
+
+        # Deleting model 'TestCase'
+        db.delete_table(u'main_testcase')
+
+        # Deleting model 'TestGrouping'
+        db.delete_table(u'main_testgrouping')
+
+        # Deleting model 'TestStepRun'
+        db.delete_table(u'main_teststeprun')
+
+        # Deleting model 'TestCaseRun'
+        db.delete_table(u'main_testcaserun')
+
+        # Deleting model 'TestGroupingRun'
+        db.delete_table(u'main_testgroupingrun')
 
 
     models = {
@@ -102,10 +209,13 @@ class Migration(SchemaMigration):
             'browser': ('django.db.models.fields.CharField', [], {'max_length': '10'}),
             'date_added': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2014, 3, 27, 0, 0)', 'auto_now_add': 'True', 'blank': 'True'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'ip_address': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
             'last_modified': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2014, 3, 27, 0, 0)', 'auto_now': 'True', 'auto_now_add': 'True', 'blank': 'True'}),
+            'overall_status': ('django.db.models.fields.CharField', [], {'default': "'Incomplete'", 'max_length': '50', 'null': 'True', 'blank': 'True'}),
             'testgrouping': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['main.TestGrouping']"}),
             'title': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
-            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']"})
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']"}),
+            'user_agent': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'})
         },
         u'main.teststep': {
             'Meta': {'object_name': 'TestStep'},
